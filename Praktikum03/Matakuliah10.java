38944b63c7adb3cfdb210e986769f908d52a0eb3
fikiazka <fikiiazka@gmail.com>
2026-02-25 08:54:31 +0700

    constructor berparameter

---
+++ Praktikum03/Matakuliah10.java
@@ -0,0 +1,20 @@
+package Praktikum03;
+
+public class Matakuliah10 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public Matakuliah10() {
+
+    }
+
+    public Matakuliah10(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+
+}
---
+++ Praktikum03/MatakuliahDemo10.java
@@ -0,0 +1,39 @@
+package Praktikum03;
+
+import java.util.Scanner;
+
+public class MatakuliahDemo10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Matakuliah10[] arrayOfMatakuliah10 = new Matakuliah10[3];
+        String kode, nama, dummy;
+        int sks, jumlahJam;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
+            System.out.print("Kode       : ");
+            kode = sc.nextLine();
+            System.out.print("Nama       : ");
+            nama = sc.nextLine();
+            System.out.print("SKS        : ");
+            dummy = sc.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam : ");
+            dummy = sc.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("--------------------------------");
+
+            arrayOfMatakuliah10[i] = new Matakuliah10(kode, nama, sks, jumlahJam);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Data Matakuliah ke-" + (i + 1));
+            System.out.println("Kode       : " + arrayOfMatakuliah10[i].kode);
+            System.out.println("Nama       : " + arrayOfMatakuliah10[i].nama);
+            System.out.println("SKS        : " + arrayOfMatakuliah10[i].sks);
+            System.out.println("Jumlah Jam : " + arrayOfMatakuliah10[i].jumlahJam);
+            System.out.println("--------------------------------");
+        }
+    }
+
+}
