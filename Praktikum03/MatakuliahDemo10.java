7586c63e2b82b0848e76ac410ac3a029cbf49c14
fikiazka <fikiiazka@gmail.com>
2026-02-25 09:37:30 +0700

    modif jumlah element

--- Praktikum03/MatakuliahDemo10.java
+++ Praktikum03/MatakuliahDemo10.java
@@ -5,28 +5,34 @@ import java.util.Scanner;
 public class MatakuliahDemo10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Matakuliah10[] arrayOfMatakuliah10 = new Matakuliah10[3];
-        String kode, nama, dummy;
+
+        System.out.print("Masukkan Jumlah Matakuliah: ");
+        int jumlahData = sc.nextInt();
+        sc.nextLine();
+
+        Matakuliah10[] arrayOfMatakuliah10 = new Matakuliah10[jumlahData];
+
+        String kode, nama;
         int sks, jumlahJam;
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < jumlahData; i++) {
+            arrayOfMatakuliah10[i] = new Matakuliah10();
             System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
             System.out.print("Kode       : ");
             kode = sc.nextLine();
             System.out.print("Nama       : ");
             nama = sc.nextLine();
             System.out.print("SKS        : ");
-            dummy = sc.nextLine();
-            sks = Integer.parseInt(dummy);
+            sks = sc.nextInt();
             System.out.print("Jumlah Jam : ");
-            dummy = sc.nextLine();
-            jumlahJam = Integer.parseInt(dummy);
+            jumlahJam = sc.nextInt();
+            sc.nextLine();
             System.out.println("--------------------------------");
 
             arrayOfMatakuliah10[i].tambahData(kode, nama, sks, jumlahJam);
         }
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < jumlahData; i++) {
             System.out.println("Data Matakuliah ke-" + (i + 1));
             arrayOfMatakuliah10[i].cetakInfo();
             System.out.println("--------------------------------");
